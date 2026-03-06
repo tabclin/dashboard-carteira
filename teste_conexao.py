6f86ae553ae4b5398fdc36d23a775922badbc40a
Thiago Buzatto <tab@tabclin.com>
2026-03-06 19:50:07 -0300

    mandando o bd para o supabase

--- teste_conexao.py
+++ teste_conexao.py
@@ -1,7 +1,12 @@
-import pandas as pd
-from sqlalchemy import create_engine
 
-DATABASE_URL = "postgresql://postgres:TabClin1706@db.hlfiykpoousspkcdswer.supabase.co:6543/postgres?sslmode=require"
+
+# DATABASE_URL = "postgresql://postgres:TabClin1706@db.hlfiykpoousspkcdswer.supabase.co:6543/postgres?sslmode=require"
+
+
+from sqlalchemy import create_engine
+import pandas as pd
+
+DATABASE_URL = "postgresql://postgres.hlfiykpoousspkcdswer:TabClin1706@aws-1-sa-east-1.pooler.supabase.com:6543/postgres"
 
 engine = create_engine(DATABASE_URL)
 
