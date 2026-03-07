a51dbc236234dd0c1a3d42b9ac3c15002c0312f9
Thiago Buzatto <tab@tabclin.com>
2026-03-07 14:36:53 -0300

    criando view 2

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -17,16 +17,21 @@ def carregar_dados():
         engine
     )
 
-    df["ultimo_atendimento"] = pd.to_datetime(
-        df["ultimo_atendimento"]
-    )
+    if df.empty:
+        return pd.DataFrame()
 
-    df["Último Atendimento"] = df["ultimo_atendimento"].dt.strftime("%d/%m/%Y")
+    df["Último Atendimento"] = pd.to_datetime(
+        df["ultimo_atendimento"],
+        errors="coerce"
+    ).dt.strftime("%d/%m/%Y")
 
     df.rename(columns={
         "paciente": "Paciente",
         "qtd_at": "Qtd At.",
-        "observacao": "Observação"
+        "recencia_dias": "Recência",
+        "status": "Status",
+        "observacao": "Observação",
+        "agendado": "Agendado"
     }, inplace=True)
 
     return df
