34bd3d5088520e251b2e1bd04c5aaac14dde8d0f
Thiago Buzatto <tab@tabclin.com>
2026-03-10 10:23:36 -0300

    rodando bem

--- automacao_api.py
+++ automacao_api.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import subprocess
+import os
 
 app = Flask(__name__)
 
@@ -21,4 +22,5 @@ def executar_importacao():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
